a94c5f3abf09fcfbad60f64ec2a2333381403bbe
C3rebraG <mr.parfentyev@gmail.com>
2025-02-24 22:28:59 +0300

    revorked parser

--- models.py
+++ models.py
@@ -2,7 +2,7 @@ from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://user:Sqlzaebal@localhost/peta"
+DATABASE_URL = "postgresql://postgres:Sqlzaebal@localhost/peta"
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
 
--- parsing_v1.py
+++ parsing_v1.py
@@ -1,38 +1,47 @@
 import json
 import time
-
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from models import Animal
 import requests
 from bs4 import BeautifulSoup
 from fake_headers import Headers
+from sqlalchemy.orm import Session
+from models import engine
+import asyncio
+import aiohttp
+from sqlalchemy.orm import sessionmaker
 
-# link = 'https://less-homeless.com/'
-link = 'https://less-homeless.com/find-your-best-friend-today/'
-headers = Headers(browser='chrome', os='win')
-req = requests.get(link, headers=headers.generate())
-time.sleep(2)
-soup = BeautifulSoup(req.text, 'lxml')
+link = 'https://less-homeless.com/find-your-best-friend-today/page/1/'
 
-cards = soup.find_all('div', class_ = 'card zs_card')
-# results = soup.find_all('div', class_='special_case__card')
-# print(card)
-headers = []
-#
-for card in cards:
-    a_tag = card.find('a', class_='card__title w-inline-block')  # Ссылка на питомца
-    img_div = card.find('div', class_='lazyload card__image')  # Блок с картинкой
-    name_tag = card.find('h2')  # Имя питомца
-    sex = card.find('div',class_='card__value').text
-    print(sex)
+def cook_soup(link):
+    headers = Headers(browser='chrome', os='win')
+    req = requests.get(link, headers=headers.generate())
+    time.sleep(2)
+    soup = BeautifulSoup(req.text, 'lxml')
+    cards = soup.find_all('div', class_='card zs_card')
+    return cards
 
-    if a_tag and 'href' in a_tag.attrs and img_div and 'data-bg' in img_div.attrs:
-        pet_name = name_tag.text.strip() if name_tag else "Неизвестно"
-        pet_url = a_tag['href']
-        image_url = img_div['data-bg']
-        # print('url!',pet_url)
-        # print('pet_name!',pet_name)
-        # print('image_url',image_url)
 
-#     header = res.find("div", class_="special_case__title").text
+def parsed_data(cards):
+    for card in cards:
+        pet_link= card.find('a', class_='card__title w-inline-block').get('href') # Ссылка на питомца
+        pet_img = card.find('div', class_='lazyload card__image').get('data-bg') # Блок с картинкой
+        pet_age = card.find('div', class_='card__value').text
+        pet_name = card.find('h2').text  # Имя питомца
+        sex = card.find('div', class_='card__value').text  # пол питомца
 
-#     headers.append(header)
-# print(headers)
+
+
+# def parsing():
+#     with Session(engine) as session:
+#         for card in cards:
+#             a_tag = card.find('a', class_='card__title w-inline-block')  # Ссылка на питомца
+#             img_div = card.find('div', class_='lazyload card__image').text  # Блок с картинкой
+#             name_tag = card.find('h2').text  # Имя питомца
+#             sex = card.find('div', class_='card__value').text  # пол питомца
+#             animal = Animal(name=name_tag, age=1, sex=sex, photo_url=img_div,
+#                             description='one')
+#             session.add(animal)
+#             session.commit()
+
+

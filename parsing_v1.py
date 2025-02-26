f09b6e94a1522f18bcc4b510d63b2e7969fb0e4a
C3rebraG <mr.parfentyev@gmail.com>
2025-02-26 06:26:22 +0300

    it is possible to insert in db

--- parsing_v1.py
+++ parsing_v1.py
@@ -10,38 +10,44 @@ from models import engine
 import asyncio
 import aiohttp
 from sqlalchemy.orm import sessionmaker
+'''DATABASE_URL в models'''
 
-link = 'https://less-homeless.com/find-your-best-friend-today/page/1/'
 
-def cook_soup(link):
-    headers = Headers(browser='chrome', os='win')
-    req = requests.get(link, headers=headers.generate())
-    time.sleep(2)
-    soup = BeautifulSoup(req.text, 'lxml')
-    cards = soup.find_all('div', class_='card zs_card')
-    return cards
+def find_cards():
+    page_num = 1
+    all_cards = []
+
+    while page_num < 15:
+        link = f'https://less-homeless.com/find-your-best-friend-today/page/{page_num}/'
+        print(page_num)
+        headers = Headers(browser='chrome', os='win')
+        req = requests.get(link, headers=headers.generate())
+        time.sleep(2)
+        soup = BeautifulSoup(req.text, 'lxml')
+        cards = soup.find_all('div', class_='card zs_card')
+        page_num +=1
+        if not cards:
+            break
+        all_cards.extend(cards)
+
+    return all_cards
 
 
 def parsed_data(cards):
-    for card in cards:
-        pet_link= card.find('a', class_='card__title w-inline-block').get('href') # Ссылка на питомца
-        pet_img = card.find('div', class_='lazyload card__image').get('data-bg') # Блок с картинкой
-        pet_age = card.find('div', class_='card__value').text
-        pet_name = card.find('h2').text  # Имя питомца
-        sex = card.find('div', class_='card__value').text  # пол питомца
+    with Session(engine) as session:
+        for card in cards:
+            pet_link= card.find('a', class_='card__title w-inline-block').get('href') # Ссылка на питомца
+            pet_img = card.find('div', class_='lazyload card__image').get('data-bg') # Блок с картинкой
+            pet_age = card.find('div', class_='card__value').text
+            pet_name = card.find('h2').text  # Имя питомца
+            sex = card.find('div', class_='card__value').text  # пол питомца
+            animal = Animal(name=pet_name, age=pet_age, sex=sex, photo_url=pet_img,
+                            description=pet_link)
+            session.add(animal)
+            session.commit()
+
+parsed_data(find_cards())
 
 
-
-# def parsing():
-#     with Session(engine) as session:
-#         for card in cards:
-#             a_tag = card.find('a', class_='card__title w-inline-block')  # Ссылка на питомца
-#             img_div = card.find('div', class_='lazyload card__image').text  # Блок с картинкой
-#             name_tag = card.find('h2').text  # Имя питомца
-#             sex = card.find('div', class_='card__value').text  # пол питомца
-#             animal = Animal(name=name_tag, age=1, sex=sex, photo_url=img_div,
-#                             description='one')
-#             session.add(animal)
-#             session.commit()
 
 
